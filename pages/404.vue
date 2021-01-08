f41fbd389e1fe31cc1415a959384820eecc44083
Siroshun09 <siro.shun8@gmail.com>
2021-01-08 21:43:10 +0900

    [Update] 404 page

---
+++ pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <NotFound />
+</template>
+
+<script>
+import NotFound from '@/components/NotFound.vue';
+export default {
+  scrollToTop: true,
+  components: {
+    NotFound
+  },
+  name: 'NotFound',
+  head() {
+    return {
+      title: 'Not Found',
+    }
+  },
+};
+</script>
