48549ad07f55941fa255112605b1dc0d7add777d
Siroshun09 <siro.shun8@gmail.com>
2021-04-20 02:57:37 +0900

    feat(footer): remove CC marks

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,34 +22,8 @@
         href="https://github.com/Siroshun09/Craftable/blob/master/LICENSE"
       >
         CC BY-SA 4.0
-        <img
-          alt="cc"
-          style="
-            height: 22px !important;
-            margin-left: 3px;
-            vertical-align: text-bottom;
-          "
-          src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
-        />
-        <img
-          alt="by"
-          style="
-            height: 22px !important;
-            margin-left: 3px;
-            vertical-align: text-bottom;
-          "
-          src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
-        />
-        <img
-          alt="sa"
-          style="
-            height: 22px !important;
-            margin-left: 3px;
-            vertical-align: text-bottom;
-          "
-          src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"
-        />
       </a>
+      .
     </small>
   </div>
 </template>
